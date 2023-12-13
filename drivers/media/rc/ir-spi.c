e1292d15b8e1829aeab2679a2e9bbcedb81cc956
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 18:27:54 +0000

    ir-spi: add support for k6a device tree

--- drivers/media/rc/ir-spi.c
+++ drivers/media/rc/ir-spi.c
@@ -275,6 +275,7 @@ static int ir_spi_remove(struct spi_device *spi)
 
 static const struct of_device_id ir_spi_of_match[] = {
 	{ .compatible = "ir-spi" },
+	{ .compatible = "qcom,spi-msm-ir" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, ir_spi_of_match);
